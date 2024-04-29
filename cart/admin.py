c06ec01880dada734d24d7066948202d33085274
Ameliah Utary <ameliah1244@gmail.com>
2024-04-29 16:05:35 +0800

    Initial commit

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['cart_id', 'create']
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['product','cart', 'quantity', 'is_active']
